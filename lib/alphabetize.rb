052afc3e81ce5863e8035d23a1aa4a32e05b2c11
Jeffrey Hechler <jeffrey.hechler@gmail.com>
2016-12-14 01:42:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,39 @@
+ESPERANTO_ALPHABET="abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  sorted = arr.sort_by do |string|
+  ESPERANTO_ALPHABET.index(string[0])
+  end
+  i = 0
+  while i < sorted.length - 1
+    #binding.pry
+      if sorted[i][0] == sorted[i + 1][0]
+        sorted = sorted.sort_by do |string|
+          ESPERANTO_ALPHABET.index(string[1])
+        end
+      end
+      i += 1
+  end
+  i = 0
+  while i < sorted.length - 1 && sorted[i].length >= 7
+      if sorted[i][4] == sorted[i + 1][4]
+        sorted = sorted.sort_by do |string|
+          #binding.pry
+          ESPERANTO_ALPHABET.index(string[6])
+        end
+      end
+      i += 1
+  end
+  i = 0
+  while i < sorted.length - 1
+      if sorted[i][4] == sorted[i + 1][4]
+        sorted = sorted.sort_by do |string|
+          #binding.pry
+          ESPERANTO_ALPHABET.index(string[5])
+        end
+      end
+      i += 1
+  end
+  sorted
 end
